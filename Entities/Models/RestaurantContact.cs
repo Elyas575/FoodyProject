a121fca85b935f3a5d0a7c50d04a1ec28dea8bc6
Farhad Zaare <z.farhad.95@gmail.com>
2021-08-30 13:50:33 +0300

    Add Two entities(mealiptions, restaurant contact)

---
+++ Entities/Models/RestaurantContact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    class RestaurantContact
+    {
+        
+        public Guid RestContactId { get; set; }
+        [Required(ErrorMessage ="Phone number should be added")]
+        public int PhoneNumber { get; set; }
+        public int RestaurantId { get; set; }
+
+
+
+    }
+}
