0f7b66d4a62cee3648cf57999143d1b890874c44
husse <husse@DUJA-SHELBAYH>
2021-08-30 13:54:04 +0300

    Custores and Orders Models

---
+++ Entities/Models/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Customer
+    {
+        [Column("CustomerId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Customer name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Coustomer  address is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Address is 60 characters")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Customer Email is a required field")]
+        [MaxLength(60, ErrorMessage = "Maximum length for Customer is 60 characters ")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "password is a required field ")]
+        [MaxLength(60, ErrorMessage = "maximun length dor passwod is 60 charchters ")]
+
+        public int  Password { get; set; }
+
+
+       
+    }
+}
---
+++ Entities/Models/Orders.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+   public  class Orders
+    {
+        [Column("OrderId")]
+
+        public Guid Orderid { get; set; }
+
+        public int  MaeilId { get; set; }
+
+
+        [Required(ErrorMessage = "Company name is a required field.")]
+        [MaxLength(200, ErrorMessage = "Maximum length for Order Description is 200 characters.")] 
+        public string OrederDescription { get; set; }
+
+
+      
+        public int MailId { get; set; }
+
+
+        public int Ammount { get; set;  }
+
+
+        public int TypeOfPayment { get; set;  }
+
+        public int TimeOfDelivery { get; set; } 
+
+        public string  Status { get; set; }
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
