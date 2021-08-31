8a8e1a93c8866cdabd31698375fbd0677bb60496
Lirah575 <elyas575@hotmail.com>
2021-08-31 10:15:22 +0300

    Connection between category and meals

--- Entities/Models/Meal.cs
+++ Entities/Models/Meal.cs
@@ -15,11 +15,14 @@ namespace Entities.Models
 
         public string Name { get; set; }
 
-
         public string Description { get; set; }
       
         public string Price { get; set; }
 
         public string Picture { get; set; }
+
+        [ForeignKey(nameof(Category))]
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
