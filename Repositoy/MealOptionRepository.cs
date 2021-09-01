3176cf022b8640894da0015210893d63f29d5d1e
Farhad Zaare <z.farhad.95@gmail.com>
2021-09-01 12:05:13 +0300

    order and mealoption repository

--- Repositoy/MealOptionRepository.cs
+++ Repositoy/MealOptionRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Contracts;
+using Entities;
+using Entities.Models;
 
 namespace Repository
 {
-    public class MealOptionRepository
+    public class MealOptionRepository : RepositoryBase<MealOption>, IMealOptionRepository
     {
+        public MealOptionRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+        {
+        }
     }
 }
--- Repositoy/OrderRepository.cs
+++ Repositoy/OrderRepository.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Contracts;
+using Entities;
+using Entities.Models;
+using Repository;
 
 namespace Repository
 {
-   public  class OrderRepository 
+   public  class OrderRepository : RepositoryBase<Order>, IOrderRepository
     {
-
-
-
-
+        public OrderRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+        {
+        }
 
     }
 }
