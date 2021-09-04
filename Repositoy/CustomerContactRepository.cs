f8ef6aa0e3201d3f55804659d50fa3b83461f32b
Farhad Zaare <z.farhad.95@gmail.com>
2021-09-04 14:28:24 +0300

    feri2

--- Repositoy/CustomerContactRepository.cs
+++ Repositoy/CustomerContactRepository.cs
@@ -16,6 +16,22 @@ namespace Repository
         : base(repositoryContext)
         {
         }
+
+        public void CreateCustomerContact(Guid customerId, CustomerContact customercontact)
+        {
+            customercontact.CustomerId = customerId;
+            Create(customercontact);
+        }
+
+        public IEnumerable<CustomerContact> GetAllCustomerContact(Guid customerId, bool trackChanges) =>
+        FindByCondition(c => c.CustomerId.Equals(customerId), trackChanges)
+        .OrderBy(c => c.CustomerAddress);
+
+
+        public CustomerContact GetCustomerContact(Guid customerId, Guid id, bool trackChanges) =>
+         FindByCondition(c => c.CustomerId.Equals(customerId) && c.CustomerContactId.Equals(id),
+        trackChanges)
+         .SingleOrDefault();
     }
 
 }
